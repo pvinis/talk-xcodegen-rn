d12aa7d2d5b906a581172148f97f8a9eca62d472
Pavlos Vinieratos <pvinis@gmail.com>
2020-08-10 15:33:48 +0200

    more

---
+++ deck/images/index.js
@@ -0,0 +1,11 @@
+import myFace2 from "./my-face2.png"
+import diff1 from "./diff1.png"
+import diff2 from "./diff2.png"
+import diff3 from "./diff3.png"
+
+export {
+	myFace2,
+	diff1,
+	diff2,
+	diff3,
+}
